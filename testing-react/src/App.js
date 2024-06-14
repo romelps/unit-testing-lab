8a20363418b015cd850dc2c65e94e079c4e3e363
Romel <romelps98@gmail.com>
2024-06-13 21:56:23 -0600

    several tests working

--- testing-react/src/App.js
+++ testing-react/src/App.js
@@ -5,35 +5,44 @@ import './App.css';
 const App =  () => {
   const [todos, setTodos] = useState([]);
   const [newToDo, setNewToDo] = useState('');
+  const [show, setShow] = useState(false);
 
   const addToDo = (evt) => {
     evt.preventDefault();
     setTodos([...todos, newToDo]);
     setNewToDo('');
+    // toggleToDo();
   }
 
   const handleChange = (event) => {
     setNewToDo(event.target.value);
   }
 
+  // const toggleToDo = () => {
+  //   setShow(true);
+  // }
+
   return <>
     <h1>Todo List</h1>
     <form>
-      <label>New To Do: </label>
+      <label htmlFor='newToDo'>New To Do: </label>
       <input 
         type="text"
         id="newToDo"
         // value={newToDo}
         onChange={handleChange}
       />
-      <button type="submit" onClick={addToDo}>Add Todo</button>
+      <button type="submit" onClick={addToDo} >Add Todo</button>
     
     <div>
+      
       <ul>
+        
         {todos.map((todo) => (
           <li id={todo.id}>{todo}</li>
-        ))}
+        ))} 
       </ul>
+    
     </div>
     </form>
   </>
--- testing-react/src/App.test.js
+++ testing-react/src/App.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders an h1 tag', () => {
@@ -7,3 +7,18 @@ test('renders an h1 tag', () => {
   expect(heading).toHaveTextContent('Todo List')
 });
 
+test('The add item label exists', () => {
+  const app = render(<App />);
+  const list = app.getByLabelText('New To Do:');
+  expect(list).toBeInTheDocument();
+})
+
+test('clicking the button displays a new todo item', () => {
+  const app = render(<App />);
+  const button = app.getByText('Add Todo');
+
+  fireEvent.click(button);
+
+  const listItems = app.getAllByRole('list item');
+  expect(listItems).toBeInTheDocument();
+});
